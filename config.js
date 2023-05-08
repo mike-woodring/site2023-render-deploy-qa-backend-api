1865edce2f3c40786f9566aacf399ffb8d3debc9
Mike Woodring <mike.woodring@codepath.org>
2023-05-08 13:18:38 -0600

    Updated getDatabaseUri to use process.env.DATABASE_HOSTED_CONNECTION_STRING if present.

--- config.js
+++ config.js
@@ -12,15 +12,22 @@ const IS_TESTING = process.env.NODE_ENV === "test"
 
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
-  const dbUser = process.env.DATABASE_USER || "postgres"
-  const dbPass = process.env.DATABASE_PASS ? encodeURI(process.env.DATABASE_PASS) : "postgres"
-  const dbHost = process.env.DATABASE_HOST || "local"
-  const dbPort = process.env.DATABASE_PORT || 5432
-  const dbTestName = process.env.DATABASE_TEST_NAME || "lifetracker_test"
-  const dbProdName = process.env.DATABASE_NAME || "lifetracker"
-  const dbName = process.env.NODE_ENV === "test" ? dbTestName : dbProdName
 
-  return process.env.DATABASE_URL || `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`
+  const hostedDatabaseConnectionString = process.env.DATABASE_HOSTED_CONNECTION_STRING;
+
+  if (hostedDatabaseConnectionString) {
+    return hostedDatabaseConnectionString;
+  } else {
+    const dbUser = process.env.DATABASE_USER || "postgres"
+    const dbPass = process.env.DATABASE_PASS ? encodeURI(process.env.DATABASE_PASS) : "postgres"
+    const dbHost = process.env.DATABASE_HOST || "local"
+    const dbPort = process.env.DATABASE_PORT || 5432
+    const dbTestName = process.env.DATABASE_TEST_NAME || "lifetracker_test"
+    const dbProdName = process.env.DATABASE_NAME || "lifetracker"
+    const dbName = process.env.NODE_ENV === "test" ? dbTestName : dbProdName
+
+    return process.env.DATABASE_URL || `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`
+  }
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
